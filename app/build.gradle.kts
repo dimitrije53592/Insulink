0574a0b59206544ef8977982ac6a5c010def8753
Jovan <jovan.pavlovic.bb@outlook.com>
2025-10-03 19:43:02 +0200

    Added missing dependencies for firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
 
     implementation(libs.navigation.compose)
 
