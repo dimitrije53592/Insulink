e61232248ca2e0dc86089ee770ce15faf4c55d8e
Jovan Pavlovic <101551013+pj210288d@users.noreply.github.com>
2025-10-07 17:25:45 +0200

    Implemented UI for LoginScreen.kt (#3)
    * Implemented UI for LoginScreen.kt
    
    * Implemented basic viewModel methods
    
    * Added missing dependencies for firebase
    
    * basic implementation of login method so it checks if user exists in firebase
    
    * changed App icon
    
    * Implemented Reset Password functionality
    
    * Changes based on the comments on the PR

--- app/src/main/java/com/dj/insulink/core/navigation/AppNavigation.kt
+++ app/src/main/java/com/dj/insulink/core/navigation/AppNavigation.kt
@@ -1,9 +1,15 @@
 package com.dj.insulink.core.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.dj.insulink.login.ui.screens.ForgotPasswordScreen
+import com.dj.insulink.login.ui.screens.ForgotPasswordScreenParams
+import com.dj.insulink.login.ui.screens.LoginScreen
+import com.dj.insulink.login.ui.screens.LoginScreenParams
+import com.dj.insulink.login.ui.viewmodel.LoginViewModel
 import com.dj.insulink.registration.ui.screens.RegistrationScreen
 
 @Composable
@@ -13,10 +19,36 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Registration.route
+        startDestination = Screen.Login.route
     ) {
         composable(Screen.Registration.route) {
             RegistrationScreen()
+        }
+        composable(Screen.Login.route) {
+            val viewModel: LoginViewModel = hiltViewModel()
+            LoginScreen(
+                params = LoginScreenParams(
+                    emailState = viewModel.email,
+                    passwordState = viewModel.password,
+                    onEmailChange = viewModel::onEmailChange,
+                    onPasswordChange = viewModel::onPasswordChange,
+                    onLogin = viewModel::login,
+                    onSignInWithGoogle = viewModel::signInWithGoogle,
+                    onForgotPasswordClicked = { navController.navigate(Screen.ForgotPassword.route) },
+                    onCheckIfUserIsLoggedIn = viewModel::checkIfUserIsLoggedIn
+                )
+            )
+        }
+        composable(Screen.ForgotPassword.route) {
+            val viewModel: LoginViewModel = hiltViewModel()
+            ForgotPasswordScreen(
+                params = ForgotPasswordScreenParams(
+                    emailState = viewModel.email,
+                    onEmailChange = viewModel::onEmailChange,
+                    onSendPasswordReset = viewModel::login,
+                    resetState = viewModel.passwordResetState
+                )
+            )
         }
     }
 }
--- app/src/main/java/com/dj/insulink/core/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/dj/insulink/core/navigation/NavigationRoutes.kt
@@ -2,4 +2,6 @@ package com.dj.insulink.core.navigation
 
 sealed class Screen(val route: String) {
     object Registration : Screen("registration")
+    object Login : Screen("login")
+    object ForgotPassword : Screen("forgot_password")
 }
