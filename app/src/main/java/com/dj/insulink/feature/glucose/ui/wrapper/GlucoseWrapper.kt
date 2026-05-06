c14752c9624e1c9a4197e465e43bb2c925863d3b
Dimitrije Ilic <dimitrijeilic@rivianvw.tech>
2026-05-06 15:18:17 +0200

    Glucose refactor

---
+++ app/src/main/java/com/dj/insulink/feature/glucose/ui/wrapper/GlucoseWrapper.kt
@@ -0,0 +1,52 @@
+package com.dj.insulink.feature.glucose.ui.wrapper
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.dj.insulink.auth.domain.models.User
+import com.dj.insulink.feature.glucose.ui.GlucoseScreen
+import com.dj.insulink.feature.glucose.ui.GlucoseScreenParams
+import com.dj.insulink.feature.glucose.ui.viewmodel.GlucoseViewModel
+
+@Composable
+fun GlucoseWrapper(
+    currentUser: User?
+) {
+    val viewModel: GlucoseViewModel = hiltViewModel()
+
+    val allGlucoseReadings = viewModel.allGlucoseReadings.collectAsStateWithLifecycle()
+    val latestGlucoseReading = viewModel.latestGlucoseReading.collectAsStateWithLifecycle()
+    val newGlucoseReadingTimestamp = viewModel.newGlucoseReadingTimestamp.collectAsStateWithLifecycle()
+    val newGlucoseReadingValue = viewModel.newGlucoseReadingValue.collectAsStateWithLifecycle()
+    val newGlucoseReadingComment = viewModel.newGlucoseReadingComment.collectAsStateWithLifecycle()
+    val showAddGlucoseReadingDialog = viewModel.showAddGlucoseReadingDialog.collectAsStateWithLifecycle()
+    val selectedTimespan = viewModel.selectedTimespan.collectAsStateWithLifecycle()
+
+    LaunchedEffect(currentUser) {
+        viewModel.fetchAllGlucoseReadingsForUserAndUpdateDatabase(currentUser?.uid)
+    }
+
+    GlucoseScreen(
+        params = GlucoseScreenParams(
+            allGlucoseReadings = allGlucoseReadings,
+            latestGlucoseReading = latestGlucoseReading,
+            selectedTimespan = selectedTimespan,
+            setSelectedTimespan = viewModel::setSelectedTimespan,
+            newGlucoseReadingTimestamp = newGlucoseReadingTimestamp,
+            setNewGlucoseReadingTimestamp = viewModel::setNewGlucoseReadingTimestamp,
+            newGlucoseReadingValue = newGlucoseReadingValue,
+            setNewGlucoseReadingValue = viewModel::setNewGlucoseReadingValue,
+            newGlucoseReadingComment = newGlucoseReadingComment,
+            setNewGlucoseReadingComment = viewModel::setNewGlucoseReadingComment,
+            showAddGlucoseReadingDialog = showAddGlucoseReadingDialog,
+            setShowAddGlucoseReadingDialog = viewModel::setShowAddGlucoseReadingDialog,
+            submitNewGlucoseReading = {
+                viewModel.submitNewGlucoseReading(currentUser?.uid)
+            },
+            deleteGlucoseReading = {
+                viewModel.deleteGlucoseReading(currentUser?.uid, it)
+            }
+        )
+    )
+}
